f6a4f2db2762955d5987df1377ddc159e2ed4b89
lenish <lenishjerone@gmail.com>
2020-02-04 11:42:50 +0530

    edited bmi

--- bmi.py
+++ bmi.py
@@ -1,4 +1,5 @@
-Height=float(input("Enter the height in metres"))
-Weight=int(input("Enter the weight in kilograms"))
-BMI=Weight/(Height*Height)
-print(BMI)
+#Calculate the body,mass and index of the user
+height=float(input("Enter the height in metres"))
+weight=int(input("Enter the weight in kilograms"))
+bmi=weight/(height*height)
+print("The BMI of the user",bmi)
